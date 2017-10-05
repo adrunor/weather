54c243f894f8b5fe68ecfdfb24689f808efb5530
Mika lgt <mika.langlet@gmail.com>
2017-10-05 22:51:06 +0200

    Update front cardview

--- app/src/main/java/local/mika/weather/MainActivity.java
+++ app/src/main/java/local/mika/weather/MainActivity.java
@@ -15,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
 		
 		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
 				== PackageManager.PERMISSION_GRANTED) {
-			mMap.setMyLocationEnabled(true);
 		} else {
 			
 		}
