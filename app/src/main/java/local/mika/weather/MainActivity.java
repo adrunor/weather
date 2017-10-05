329402e813ba81ed7ce3dbe598f65c98ac6d2419
Mickael LANGLET <mlanglet@wynd.eu>
2017-10-05 14:00:10 +0200

    start project

---
+++ app/src/main/java/local/mika/weather/MainActivity.java
@@ -0,0 +1,23 @@
+package local.mika.weather;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+				== PackageManager.PERMISSION_GRANTED) {
+			mMap.setMyLocationEnabled(true);
+		} else {
+			
+		}
+	}
+}
